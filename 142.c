6cc00182794744b7115894bf6d64db1a45229374
chengke <chengkechengke@gmail.com>
2013-04-24 20:51:17 +0800

    problem 142, another submit
     got "Runtime Error on test 8"
    
     use 0,1 to replace a,b in input string s
     map string to int
     mark[i][v] means in string s, substring with length i, value v exist;

--- 142.c
+++ 142.c
@@ -3,30 +3,34 @@
 #include <stdio.h>
 #include <string.h>
 
+int n ;
 char s[500002] = "";
-char ans[22] = "";
+char mark[20][524290];
+int result;
+char ans[21];
 
-/*
-int next(char* str) {
-  int i = 0;
-  while (str[i] != '\0') {
-    if (str[i] == 'a') {
-      str[i] = 'b';
-      return 1;
-    }
-    else {
-      str[i] = 'a';
-      ++i;
+void initMark(int p) {
+  memset(mark, sizeof(mark), 0);
+  int i;
+  for (i=0; i<n; ++i) {
+    s[i] -= 'a';
+  }
+  
+  for (i=0; i<n; ++i) {
+    int v = 0;
+    int j = 1;
+    while (j<=p && i+j-1<n) {
+      v = (v<<1) | s[i+j-1];
+      mark[j][v] = 1;
+      ++j;
     }
   }
-  return 0;
 }
-*/
 
 //convert int v to binary string in length len
 void int2str(int v, int len, char *str) {
-  int i = 0;
-  for (; i<len; ++i) {
+  int i = len - 1;
+  for (; i>=0; --i) {
     str[i] = 'a' + (v & 1);
     v >>= 1;
   }
@@ -45,12 +49,11 @@ int contain(int len) {
   int i = 0;
   int b = (1 << len);
   for ( ; i<b; ++i) {
-    int2str(i, len, ans);
-    if (strstr(s, ans) == NULL) {
+    if (mark[len][i] == 0) {
+      result = i;
       return 0;
     }
   }
-
   return 1;
 }
 
@@ -60,12 +63,12 @@ int main() {
   freopen("in.txt", "r", stdin);
   #endif
 
-  int n;
   scanf("%d%s",&n, s);
 
   int low = 0;
   int up = LOG2(n) + 1;
   int mid;
+  initMark(up);
   while (up - low > 1) {
     mid = (up + low) / 2;
     if (contain(mid)) {
@@ -75,11 +78,12 @@ int main() {
       up = mid;
     }
   }
-  if (up != mid) {
+  if (up == LOG2(n)+1) {
     contain(up);
   }
   
-  printf("%d\n%s\n", strlen(ans), ans);
+  int2str(result, up, ans);
+  printf("%d\n%s\n", up, ans);
   return 0;
 }
 
