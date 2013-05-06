46c66c4a32c1a3cfe3d06f2451250c6fa2f852d8
chengke <chengkechengke@gmail.com>
2013-05-06 16:03:17 +0800

    problem 186, second submit, got "Wrong answer on test 20"

--- 186/sgu.c
+++ 186/sgu.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int n;
 int c[101] = { 0 };     // c[Li] is the count of links with length Li
 int ac[101] = { 0 };    //ac[Li] is the count of links whith length <= Li
 int b[101] = { 0 };     // b[Li] is the Link length less than Li;
-char f[50][52] ;
+char f[101][101] ;
 
-int before(int v) {
-  int k = v - 1;
-  while (c[k] <= 0) --k;
-  return k;
-}
 
-//change recusive to loop
 int test(int m, int v) {
+  if (f[m][v] != -1) {
+    return f[m][v];
+  }
   if (m == 1 && c[v] > 0) {
-    return 1;
+    f[m][v] = 1;
+    return f[m][v];
   }
   if (ac[v] < m) {
-    return 0;
+    f[m][v] = 0;
+    return f[m][v];
   }
-
   if (b[v] == 0) {
-    return 0;
-  }
-  return test(m-1, v-b[v]) || test(m, b[v]);
+    f[m][v] = 0;
+    return f[m][v];
+  }  
+  f[m][v] = test(m-1, v-b[v]) || test(m, b[v]);
+  return f[m][v];
 }
 
 
@@ -50,7 +51,7 @@ int main() {
       b[i] = j;
     }
   }
-
+  memset(f, -1, sizeof(f));
   for (i=(n-1)/2; i>0; --i) {
     if (test(i, n-i-1)) {
 	break;
