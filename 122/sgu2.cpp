c975d6820704fe209e8de334be749ff7b131032e
ck <chengkechengke@gmai.com>
2013-06-15 10:29:26 +0800

    problem 122,got 'Time Limit Exceeded on test 10 	515 ms	1887 kb'

--- 122/sgu2.cpp
+++ 122/sgu2.cpp
@@ -1,22 +1,15 @@
-#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <vector>
 #include <fstream>
 
 using namespace std;
 
 const int SIZE = 1002;
-int n;
-vector<int> g[SIZE];  //graph representation by adjacency list
+bool adj[SIZE][SIZE] = { {false} };
 bool visited[SIZE] = { false };  
-int next[SIZE], start, tail, len;
+int n, pre[SIZE], next[SIZE], start, tail, len;
 
-/*************************
-description:
- - read input
-***********************/
 void read()
 {
   #ifndef ONLINE_JUDGE
@@ -30,141 +23,115 @@ void read()
   istringstream sin(str);
   sin >> n;
   int i, a;
-  
   for (i=1; i<=n; ++i) {
     getline(cin, str);
     istringstream sin(str);
     while (sin >> a){
-      g[i].push_back(a);
+      adj[a][i] = true; 
+      adj[i][a] = true;
     }
   }
 }
 
-
-bool is_adj(int a, int b)
-{
-  vector<int>::iterator it;
-  it = find(g[a].begin(), g[a].end(), b);
-  if (it != g[a].end()) {
-    return true;
+int find_unvisited_child(int v) {
+  int i = 1;
+  for (; i<=n; ++i) {
+    if (adj[v][i] && !visited[i]) {
+      return i;
+    }
   }
-  
-  return false;
+  return 0;
+}
+
+int find_visited_child(int v) {
+  int i = start;
+  while (i != tail) {
+    if (adj[v][i]) return i;
+  }
+  if (adj[v][tail]) return tail;
+  return 0;
 }
 
 void find_one_path() 
 {
-  start = tail = 1;
-  int v ;
-  int i;
-  bool over = false;
-  //forword
-  v = 1;
+  len = start = tail = 1;
   visited[1] = true;
-  len = 1;
-  while (!over) {
-    over = true;
-    for (i=0; i<g[v].size(); ++i) {
-      if (!visited[g[v][i]]) {
-	tail = g[v][i];
-	next[v] = tail;
-	visited[tail] = true;
-	++len;
-	v = tail;
-	over = false;
-	break;
-      }
-    }
+  //forword
+  int u, v = 1;
+  while ((u=find_unvisited_child(v)) != 0) {
+    next[v] = u;
+    pre[u] = v;
+    ++len;
+    visited[u] = true;
+    v = u;
+    tail = u;
   }
-
   //back
   v = 1;
-  over = false;
-  while (!over) {
-    over = true;
-    for (i=0; i<g[v].size(); ++i) {
-      if (!visited[g[v][i]]) {
-	start = g[v][i];
-	next[start] = v;
-	visited[start] = true;
-	++len;
-	v = start;
-	over = false;
-	break;
-      }
-    }
+  while((u=find_unvisited_child(v)) != 0) {
+    next[u] = v;
+    pre[v] = u;
+    ++len;
+    visited[u] = true;
+    v = u;
+    start = u;
   }
 }
 
 void output() 
 {
-  int v ;
-  for (v=1; v!=tail; v=next[v]) {
+  int v = 1;
+  do {
     cout << v << " ";
-  }
-  cout << tail << " ";
-  for (v=start; v!=1; v=next[v]) {
-    cout << v << " ";
-  }
+    v = next[v];
+  }while (v != 1);
   cout << 1 << endl;
 }
 
-void find_node_outside_path(int &z, int &k)
+void extend_path()
 {
   int i, j;
-  for (i=1; i<=n; ++i) {
+  for (i=1; i<=n ; ++i) {
     if (!visited[i]) {
-      for (j=0; j<g[i].size(); ++j) {
-	if (visited[g[i][j]]) {
-	  z = i;
-	  k = g[i][j];
-	  return;
-	} 
+      j = find_visited_child(i);
+      if (j != 0) {
+	break;
       }
     }
   }
-}
 
-void extend_path(int z, int k)
-{
-  next[tail] = start;
-  next[z] = k;
-  visited[z] = true;
+  start = i;
+  tail = pre[j];
+  next[i] = j;
+  pre[j] = i;
+  next[tail] = 0;
+  visited[i] = true;
   ++len;
-  start = z;
-  tail = start;
-  while (next[tail] != k) tail = next[tail];
 }
 
-void find_loop_node(int& k) 
+void path2loop() 
 {
-  k = next[start];
-  while (k != tail) {
-    if (is_adj(k, tail) && is_adj(next[k], start)) {
-      return;
+  int i = next[start];
+  int j = next[i];
+  while (j != tail) {
+    if (adj[i][tail] && adj[j][start]) {
+      break;
+    }
+    else {
+      i = j;
+      j = next[i];
     }
   }
-}
 
-void path2loop(int k) 
-{
-  int v = next[k];
-  int pre[SIZE], sp, tp;
-  sp = tail;
-  tp = v;
-  while (next[v] != tail) {
-    pre[next[v]] = v;
-    v = next[v];
+  while (tail != j) {
+    int tmp = pre[tail];
+    next[i] = tail;
+    pre[tail] = i;
+    i = tail;
+    tail = tmp;
   }
-  pre[tail] = v;
-
-  next[k] = sp;
-  tail = tp;
-  v = sp;
-  while (v != tp) {
-    next[v] = pre[v];
-    v = pre[v];
-  }
+  next[i] = tail;
+  pre[tail] = i;
 }
 
 void solve() 
@@ -172,21 +139,19 @@ void solve()
   find_one_path();
   
   while (true) {
-    if (is_adj(start, tail)) {
+    if (adj[start][tail]) {
+      next[tail] = start;
+      pre[start] = tail;
       if (len == n) {
 	output();
 	return;
       }
       else {
-	int z, k;
-	find_node_outside_path( z, k);
-	extend_path(z, k);
+	extend_path();
       }
     }
     else {
-      int k;
-      find_loop_node( k);
-      path2loop( k);
+      path2loop();
     }
   }
 }
