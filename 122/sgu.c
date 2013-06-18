62b87cc8fb741c97fb37e35aba3ecf209a5ed0ee
ck <chengkechengke@gmai.com>
2013-06-18 14:30:25 +0800

    problem 122, got 'Time Limit Exceeded on test 13 750ms	1827kb'  - delte some functions, move them to function main;

--- 122/sgu.c
+++ 122/sgu.c
@@ -6,31 +6,9 @@ char adj[SIZE][SIZE] = {{0}};
 char visited[SIZE] = { 0 };  
 int n,  next[SIZE], start, tail, len;
 
-void read()
-{
-  scanf("%d", &n);
-  getchar();
-  
-  int i, a;
-  for (i=1; i<=n; ++i) {
-    while(1) {
-      scanf("%d", &a);
-      adj[i][a] = adj[a][i] = 1;
-      a = getchar();
-      if(a=='\n' || a=='\r' || a==EOF)	break;
-    }   
-  }
-}
-
-int find_unvisited_child(int v) {
-  int i = 1;
-  for (; i<=n; ++i) {
-    if (adj[v][i] && !visited[i]) {
-      return i;
-    }
-  }
-  return 0;
-}
+int i, j, a;
+int tmp[SIZE], c;
+char flag;
 
 int find_visited_child(int v) {
   int i = start;
@@ -44,111 +22,117 @@ int find_visited_child(int v) {
 void find_one_path() 
 {
   //forword
-  int u, v = tail;
-  while ((u=find_unvisited_child(v)) != 0) {
-    next[v] = u;
-    ++len;
-    visited[u] = 1;
-    v = u;
-    tail = u;
+  a = tail;
+  flag = 1;
+  while (flag) {
+    flag = 0;
+    for (i=1; i<=n; ++i) {
+      if (adj[a][i] && !visited[i]) {
+	next[a] = i;
+	++len;
+	flag = visited[i] = 1;
+	a = tail = i;
+      }    
+    }
   }
+  
   //back
-  v = start;
-  while((u=find_unvisited_child(v)) != 0) {
-    next[u] = v;
-    ++len;
-    visited[u] = 1;
-    v = u;
-    start = u;
-  }
-}
-
-void output() 
-{
-  int v = 1;
-  do {
-    printf("%d ", v);
-    v = next[v];
-  }while (v != 1);
-  printf("1 \n");
-}
-
-void extend_path()
-{
-  int i, j;
-  for (i=1; i<=n ; ++i) {
-    if (!visited[i]) {
-      j = find_visited_child(i);
-      if (j != 0) {
-	break;
-      }
-    }
-  }
-  start = next[j];
-  tail = i;
-  next[j] = i;
-  visited[i] = 1;
-  ++len;
-  find_one_path();
-}
-
-void path2loop() 
-{
-  int i = next[start];
-  int j = next[i];
-  while (j != tail) {
-    if (adj[i][tail] && adj[j][start]) {
-      break;
-    }
-    else {
-      i = j;
-      j = next[i];
-    }
-  }
-  
-  int tmp[SIZE];
-  int p = j;
-  int c = 0;
-  do {
-    tmp[c] = p;
-    ++c;
-    p = next[p];
-  }while (p != tail);
-  next[i] = tail;
-  i = tail;
-  for (--c; c>=0; --c) {
-    next[i] = tmp[c];
-    i = tmp[c];
-  }
-  tail = i;
-}
-
-void solve() 
-{
-  len = start = tail =  visited[1] = 1;
-  find_one_path();
-  
-  while (1) {
-    if (adj[start][tail]) {
-      next[tail] = start;
-      if (len == n) {
-	output();
-	return;
-      }
-      else {
-	extend_path();
-      }
-    }
-    else {
-      path2loop();
+  a = start;
+  flag = 1;
+  while (flag) {
+    flag = 0;
+    for (i=1; i<=n; ++i) {
+      if (adj[a][i] && !visited[i]) {
+	next[i] = a;
+	++len;
+	flag = visited[i] = 1;
+	a = start = i;
+      }    
     }
   }
 }
 
 int main() 
 {
-  read();
-  solve();
+  // read
+  scanf("%d", &n);
+  getchar();
+  for (i=1; i<=n; ++i) {
+    while(1) {
+      scanf("%d", &a);
+      adj[i][a] = adj[a][i] = 1;
+      a = getchar();
+      if(a=='\n' || a=='\r' || a==EOF)	break;
+    }   
+  }
+  
+  // solve 
+  len = start = tail =  visited[1] = 1;
+  // find a path
+  find_one_path();
+  
+  while (1) {
+    if (adj[start][tail]) {
+      next[tail] = start;
+      if (len == n) {
+	//output result
+	i = 1;
+	do {
+	  printf("%d ", i);
+	  i = next[i];
+	}while (i != 1);
+	printf("1 \n");
+	return 0;
+      }
+      else {
+	//extend path
+	for (i=1; i<=n ; ++i) {
+	  if (!visited[i]) {
+	    j = find_visited_child(i);
+	    if (j != 0) {
+	      break;
+	    }
+	  }
+	}
+	start = next[j];
+	tail = i;
+	next[j] = i;
+	visited[i] = 1;
+	++len;
+	find_one_path();
+
+      }
+    }
+    else {
+      //path to cycle
+      i = next[start];
+      j = next[i];
+      while (j != tail) {
+	if (adj[i][tail] && adj[j][start]) {
+	  break;
+	}
+	else {
+	  i = j;
+	  j = next[i];
+	}
+      }
+      a = j;
+      c = 0;
+      do {
+	tmp[c] = a;
+	++c;
+	a = next[a];
+      }while (a != tail);
+      next[i] = tail;
+      i = tail;
+      for (--c; c>=0; --c) {
+	next[i] = tmp[c];
+	i = tmp[c];
+      }
+      tail = i;
+    }
+  }
+
   return 0;
 }
 
