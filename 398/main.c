fc8c94dc239faa0fe3e5394552512b747dcc71ae
cheky <chengkechengke@gmail.com>
2014-07-13 17:07:02 +0800

    submit second time, got "Wrong answer on test 15, 15 ms, 1279 kb".

--- 398/main.c
+++ 398/main.c
@@ -5,43 +5,28 @@
 int n, x;
 int a[MAX][MAX] = { 0 };
 int b[MAX] = { 0 };
-int s[MAX], sp = 0;
-int visited[MAX] = { 0 };
-int i, j;
+int i, j, d;
 
 int main() {
   scanf("%d%d", &n, &x);
   for (i=1; i<=n; ++i) {
     scanf("%d", &a[i][0]);
     for (j=1; j<=a[i][0]; ++j) {
-      scanf("%d", &a[i][j]);
+      scanf("%d", &d);
+      a[d][i] = a[i][d] = 1;
     }
   }
   
-  visited[x] = 1;
-  b[x] = -1;
-  for (i=1; i<=a[x][0]; ++i) {
-    s[sp++] = a[x][i];
-    b[a[x][i]] = -1;
-  }
-
-  while (sp > 0) {
-    j = s[--sp];
-    if (visited[j]) continue;
-
-    for(i=1; i<=a[j][0]; ++i) {
-      if (b[a[j][i]] == 0){
-	b[a[j][i]] = 1;
-	++b[0];
+  for (i=1; i<=n; ++i) 
+    for (j=1; j<=n; ++j) {
+      if (x!=i && x!=j && i!=j && a[x][i] && a[i][j] && !a[x][j]) {
+	b[++b[0]] = j;
       }
-      if (!visited[a[j][i]]) s[sp++] = a[j][i];
     }
-    visited[j] = 1;
-  }
 
   printf("%d\n", b[0]);
-  for (i=1; i<=n; ++i) {
-    if (b[i]==1) printf("%d ", i);
+  for (i=1; i<=b[0]; ++i) {
+    printf("%d ", b[i]);
   }
   putchar('\n');
   return 0;
